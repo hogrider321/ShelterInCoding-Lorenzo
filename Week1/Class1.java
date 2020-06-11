72619da044603c5cdb940af7699cb228fe16a5c9
hogrider321 <66787886+hogrider321@users.noreply.github.com>
2020-06-11 10:30:42 -0400

    Add files via upload

---
+++ Week1/Class1.java
@@ -0,0 +1,30 @@
+public class test {
+	public static void main(String[] args) {
+		//System.out.println("Hi")
+
+    	//int a = 1;
+    	//int b = 2;
+    	//System.out.println("here is a:" + a);
+    	//int sum = a + b;
+    	//System.out.println("This is the sum: " + sum);
+
+    	//double c = 3.5;
+    	//System.out.println("Here is c: " + c);
+    	//c = 5.6;
+    	//System.out.println("Here is c: " + c);
+
+    	// int a = 1;
+    	// int b = 2;
+    	// System.out.println("Here is a: " + a);
+    	// a = 2;
+    	// System.out.println("Here is a: " + a);
+    	// int sum = a + b;
+    	// System.out.println("This is the sum: " + sum);
+
+    	// double c = 3.5;
+    	// double d = 3.5;
+    	// double product = c * d;
+    	// System.out.println(product);
+    	// System.out.println(c / d);
+	}
+}
---
+++ Week1/Class1.py
@@ -0,0 +1,26 @@
+print("sup")
+
+a = "Lorenzo"
+b = 19
+print(a)
+print(b)
+
+age = 20
+print(age) 
+
+year = 2020
+year_born = 2004
+age = year - year_born
+print(age)
+
+pi = 3.14
+print(pi)
+
+print(type(a))
+
+print("Here are my variables {} {}".format(b,a))
+
+item1 = "apples"
+quantity1 = 5
+print("I have some {}. How many would you like? I have {}".format(item1, quantity1))
+print("I have " + str(quantity1) + " apples")
